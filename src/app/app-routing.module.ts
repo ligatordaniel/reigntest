c4eeca0a619b4ece219ce788c8b7be3d39d6a208
haibulinux <daniel.ulloa@haibu.cl>
2022-01-31 21:50:25 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'news', loadChildren: () => import('./modules/news/pages.module').then(m => m.PagesModule) }, 
+
+  { path: 'favorites', loadChildren: () => import('./modules/favorites/pages.module').then(m => m.PagesModule) },
+
+  { path: 'about', loadChildren: () => import('./modules/about/pages.module').then(m => m.PagesModule) }, 
+
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
